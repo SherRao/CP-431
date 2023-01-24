9cb26dbe54797ed127597540dc5f205a8926e16c
Nausher Rao <SherRao15@gmail.com>
2023-01-24 01:22:48 -0500

    Add output folder to output flag

--- Assignment1/start_job.sh
+++ Assignment1/start_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=2
 #SBATCH --time=00:01:00
 #SBATCH --job-name mpi_job
-#SBATCH --output=mpi_output_%j.txt
+#SBATCH --output=output/mpi_output_%j.txt
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=raox6250@mylaurier.ca
 
