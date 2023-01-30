6a76e30c399f2c27cfdfb37df38f3883955dd554
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-01-29 23:57:31 -0500

    Update formatting

--- Assignment1/serial_handin.c
+++ Assignment1/serial_handin.c
@@ -15,7 +15,6 @@ int SieveOfEratosthenes(int n, int lst[])
     bool prime[flr + 1];
     memset(prime, true, sizeof(prime));
 
-
     int x = 0;
     int c = 0;
 
@@ -23,7 +22,7 @@ int SieveOfEratosthenes(int n, int lst[])
     {
         if (prime[p] == true)
         {
-            for (int i = p * p; i <= flr; i += p){
+            for (int i = p * p; i <= flr; i += p) {
                 prime[i] = false;
                 //lst[i] = false;
             }
@@ -32,7 +31,7 @@ int SieveOfEratosthenes(int n, int lst[])
     }
 
     for (int p = 2; p <= flr; p++)
-        if (prime[p]){
+        if (prime[p]) {
             lst[c] = p;
             c++;
         }
@@ -42,37 +41,35 @@ int SieveOfEratosthenes(int n, int lst[])
 
 int main()
 {
-        int n = 100000000;
-        int largest_gap = 0;
-        int G = 0;
+    int n = 100000000;
+    int largest_gap = 0;
+    int G = 0;
 
-        float sqt = sqrt(n);
-        int flr = floor(sqt) + 1;
-        
-        printf("This is the biggest gap from one to %d \n", n);
-        int x[flr+1];
+    float sqt = sqrt(n);
+    int flr = floor(sqt) + 1;
+    
+    printf("This is the biggest gap from one to %d \n", n);
+    int x[flr+1];
 
-        memset(x, 0, sizeof x);
+    memset(x, 0, sizeof x);
 
+    int count = SieveOfEratosthenes(n, x);
 
-        int count = SieveOfEratosthenes(n, x);
+    int block_size = 10000;
+    for (int i = block_size; i < n; i += block_size){
+    if (i > n == false)
+        G = segmentedSieve(block_size + i - 1, x, count, i);
 
-        int block_size = 10000;
-        for (int i = block_size; i < n; i += block_size){
-        if (i > n == false)
-            G = segmentedSieve(block_size + i - 1, x, count, i);
-
-            if (G > largest_gap)
-            {
-                largest_gap = G;
-                G = 0;
-            }
+        if (G > largest_gap)
+        {
+            largest_gap = G;
+            G = 0;
         }
-        printf("%d", largest_gap);
+    }
+    printf("%d", largest_gap);
 
     return 0;
 }
-
 
 int segmentedSieve(int n, int first_primes[], int count, int start)
 {
@@ -87,45 +84,40 @@ int segmentedSieve(int n, int first_primes[], int count, int start)
     bool block[n - start + 1];//limit + 1];
     memset(block, true, sizeof(block));
 
-    
-    
     int low = limit;
     int high = 2 * limit;
     int c = 0;
 
     while (low < n)
     {
-            if (high >= n)
-                high = n;
-            c++;
+        if (high >= n)
+            high = n;
+        c++;
 
-            for (int i = 0; i < count; i++)
-            {
-                float temp = ((float)low) / ((float)first_primes[i]);
-                int lowest_lim = floor(temp) * first_primes[i];
+        for (int i = 0; i < count; i++)
+        {
+            float temp = ((float)low) / ((float)first_primes[i]);
+            int lowest_lim = floor(temp) * first_primes[i];
 
-                if (lowest_lim < low)
-                lowest_lim += first_primes[i];
+            if (lowest_lim < low)
+            lowest_lim += first_primes[i];
 
-                
-                for (int j = lowest_lim; j < high; j += first_primes[i])
-                block[j - low] = false;
+            for (int j = lowest_lim; j < high; j += first_primes[i])
+            block[j - low] = false;
+        }
+
+        for (int i = low; i < high; i++)
+        {
+            if (block[i - low] == true){
+                if (max_gap < gap)
+                    max_gap = gap;
+                    gap = 0;
             }
+            gap++;
+        }
 
-
-            for (int i = low; i < high; i++)
-            {
-                if (block[i - low] == true){
-                    if (max_gap < gap)
-                        max_gap = gap;
-                        gap = 0;
-                }
-                gap++;
-            }
-
-
-            low = low + limit;
-            high = high + limit;
+        low = low + limit;
+        high = high + limit;
     }
 
     return max_gap;
