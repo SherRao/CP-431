8574538f5418a1680d4d855d28bb35d5b3c7f78a
Zamil Bahri <zamilbahri@gmail.com>
2023-02-25 16:51:08 -0500

    working version, disorganized

--- Assignment2/src/algo.py
+++ Assignment2/src/algo.py
@@ -1,7 +1,7 @@
 import numpy as np
 from mpi4py import MPI
 
-n = 11
+n = 1000000
 
 
 comm = MPI.COMM_WORLD
@@ -15,6 +15,12 @@ if rank == 0:
     b = np.random.randint(0, 5*n, size=n, dtype='i')
     b = np.sort(b)
 
+    # a = np.array([8,11,19,30,34,34,37,38,42,43,53], dtype='i')
+    # b = np.array([3,4,7,10,17,26,28,32,35,42,44], dtype='i')
+
+    # print(a)
+    # print(b)
+
     # Split the list into parts based on number of cores 
     a_s = np.array_split(a, size - 1)
     
@@ -26,7 +32,8 @@ if rank == 0:
     # O(n) search for break points in B to create blocks
     space = [0]
     c = 0
-    for i in a_s[0:size - 2]:
+
+    for i in a_s:
         for j in b[c:]:
             if j > i[-1]:
                 space.append(c)
@@ -36,11 +43,24 @@ if rank == 0:
 
     for i in range(1, size):
         # tag 1 is a B list block
-        comm.send(len(b[space[i-1]:space[i]-1]), dest=i, tag=4)
-        comm.Send([b[space[i-1]:space[i]-1], MPI.INT], dest=i, tag=1)
-        print(i)
+        comm.send(len(b[space[i-1]:space[i]]), dest=i, tag=4)
+        comm.Send([b[space[i-1]:space[i]], MPI.INT], dest=i, tag=1)
 
-    print("sent", b, space)
+    # print("sent", b, space)
+    
+    # receive merged arrays.
+    merged_arrays = [None] * (size-1)
+    for i in range(1,size):
+        # tag 5 is length of merged array, tag 6 is the merged array
+        size = comm.recv(source=i, tag=5) 
+        merged = np.empty( size, dtype='i')
+        comm.Recv([merged, MPI.INT], source=i, tag=6)
+        merged_arrays[i-1] = merged
+    
+    merged_arrays = np.concatenate(merged_arrays, dtype = 'i')
+    # print(merged_arrays)
+    print("Done")
+
 
 elif rank >= 1:
     # Get an A block, first size then numpy array
@@ -53,6 +73,26 @@ elif rank >= 1:
     datab = np.empty(y, dtype='i')
     comm.Recv([datab, MPI.INT], source=0, tag=1)
 
-    print(data, " ", datab, " ", rank)
+    # Merge data and datab
+    i=j=k=0
+    merged = np.empty(x+y, dtype='i')
+    
+    while i < x and j < y:
+      if data[i] < datab[j]:
+        merged[k] = data[i]
+        i += 1
+      else:
+        merged[k] = datab[j]
+        j += 1
+      k += 1
+
+    merged[k:] = data[i:]
+
+    # print(data, " ", datab, " ", rank)
+    # print(merged)
+
+    # tag 5 is length of merged array, tag 6 is the merged array
+    comm.send(merged.size, dest=0, tag=5) 
+    comm.Send([merged, MPI.INT], dest=0, tag=6)
 
 
