816c09a8ed11cc61c04b5ffbce19ee70b0fdd490
Met <memetrusidovski@gmail.com>
2023-02-26 20:07:23 -0500

    new

--- Assignment2/src/sort.pyx
+++ Assignment2/src/sort.pyx
@@ -3,9 +3,9 @@ from cpython cimport array
 import numpy 
 cimport numpy
 
-
+# Sort custom sort function which compiles to C for optimal performance 
 ctypedef numpy.ndarray DTYPE_t
-@cython.boundscheck(False) # turn off bounds-checking for entire function
+@cython.boundscheck(False)
 @cython.wraparound(False)
 def sort(numpy.ndarray[int, ndim=1] data, numpy.ndarray[int, ndim=1] datab, int x, int y):
     cdef int i = 0
@@ -46,7 +46,7 @@ cpdef list split(int size, numpy.ndarray[int, ndim=1] b, a_s):
                 breaks_index += 1
                 break
             b_index += 1
-        
+    
         #for b_elem in b[b_index:]:
         #    if b_elem > group[-1]:
         #        breaks[breaks_index] = b_index
@@ -57,6 +57,23 @@ cpdef list split(int size, numpy.ndarray[int, ndim=1] b, a_s):
     breaks[-1] = len(b)
 
     return breaks
+
+def binary_search():
+    first = 0
+    last = len(arr)
+    mid = int((first + last)/2)
+    
+    while first <= last:
+        if arr[mid] == elem:
+            return mid
+        elif arr[mid] < elem:
+            first = mid + 1
+        else:
+            last = mid - 1
+
+        mid = int((first + last)/2)
+
+    return mid + 1
 
 def test(x):
     cdef int y = 5
