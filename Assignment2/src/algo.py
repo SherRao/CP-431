9ba8efbb5cb3ad33bb2f41f4beb3361d78d8ee09
Zamil Bahri <zamilbahri@gmail.com>
2023-02-26 19:52:35 -0500

    little bit of cleanup

--- Assignment2/src/algo.py
+++ Assignment2/src/algo.py
@@ -1,10 +1,11 @@
 import numpy as np
 from mpi4py import MPI
-import sys
+import sys, gc
 
-BIG_N = 1_000_000_000 # The number of integers
+BIG_N = 100_000_000 # The number of integers
 MAX_INT = 10 * BIG_N # The range in which random ints are generated
 PRINT_LIMIT = 20 # Don't print arrays if size exceeds this value
+CHECK_CORRECTNESS = False
 
 SEED = 50 # For numpy array generation
 np.random.seed(SEED)
@@ -33,6 +34,9 @@ def binary_search(arr, elem, start):
 def generate_sorted_randint_array(lower, upper, n, dtype):
     return np.sort(np.random.randint(lower, upper, size=n, dtype=dtype))
 
+def simple_merge(arr1, arr2):
+    return np.sort(np.concatenate((arr1,arr2)), kind='merge')
+
 if rank == 0:
     # Generate to randomly sorted lists
     print(f"N = {BIG_N:,}")
@@ -48,6 +52,7 @@ if rank == 0:
         print("A:      ", a)
         print("B:      ", b)
 
+    # Start merge timer
     merge_start_time = MPI.Wtime()
 		
     # Split the list into parts based on number of cores
@@ -59,14 +64,13 @@ if rank == 0:
         comm.Send([ a_s[i-1] , MPI.INT], dest=i, tag=0)
     
     # O(n) search for break points in B to create blocks
-    # initialize all elements to be length of b, and set first element to be 0
-    breaks = [len(b)]*size 
-    breaks[0] = 0
+    breaks = [0]*size 
 
     for i, group in enumerate(a_s):
         b_index = binary_search(b, group[-1], breaks[i])
         breaks[i+1] = b_index
 
+    # Set last element to be the length of the array
     breaks[-1] = len(b)
     
     for i in range(1, size):
@@ -74,6 +78,13 @@ if rank == 0:
         lower, upper = breaks[i-1], breaks[i]
         comm.send(len(b[lower:upper]), dest=i, tag=4)
         comm.Send([b[lower:upper], MPI.INT], dest=i, tag=1)
+
+    # delete a and b array to save memory and prevent spikes in RAM usage in the next part
+    #   Doesn't work as intended :(
+    # if (not CHECK_CORRECTNESS):
+    #     del a
+    #     del b
+    #     gc.collect()
     
     # receive merged arrays.
     merged_arrays = [None] * (size-1)
@@ -84,19 +95,23 @@ if rank == 0:
         comm.Recv([merged, MPI.INT], source=i, tag=6)
         merged_arrays[i-1] = merged
     
+    # concatenate all merged arrays into one
     merged_arrays = np.concatenate(merged_arrays, dtype = 'int64')
+    print(f'Size of merged array: {sys.getsizeof(merged_arrays)/1_000_000: ,.2f} MB')
 
+    # end merge timer
     merge_end_time = MPI.Wtime()
     
-    
+    # Print final results and time
     if (BIG_N < PRINT_LIMIT):
-        print("Merged: ", merged_arrays, merged_arrays.shape)
+        print("Merged: ", merged_arrays)
     print(f"Time to parallel merge = {merge_end_time - merge_start_time: .4f} seconds")
     
-    print("Checking for correctness:", end=" ")
-    simple_merged = np.sort(np.concatenate((a,b)), kind='merge')
-    correct = "Correct" if ((merged_arrays==simple_merged).all()) else "Incorrect"
-    print(correct)
+    # Check for correctness
+    if (CHECK_CORRECTNESS):
+        print("Checking for correctness:", end=" ")
+        correct = "Correct" if ((merged_arrays==simple_merge(a,b)).all()) else "Incorrect"
+        print(correct)
 
 
 elif rank >= 1:
@@ -111,10 +126,8 @@ elif rank >= 1:
     comm.Recv([datab, MPI.INT], source=0, tag=1)
 
     # Merge data and datab
-    merged = np.sort(np.concatenate((data,datab)), kind='merge')
-    # print(data)
-    # print(datab)
-    # print(merged)
+    merged = simple_merge(data, datab)
+
     # tag 5 is length of merged array, tag 6 is the merged array
     comm.send(merged.size, dest=0, tag=5) 
     comm.Send([merged, MPI.INT], dest=0, tag=6)
